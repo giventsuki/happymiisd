5330328acd1d14fee254b1d3853ee39069d695d1
tsukinom1 <example@gmail.com>
2024-10-29 21:31:29 +0500

    Deploy React app to GitHub Pages

--- WebstormProjects/happybd/src/components/ImageCard/ImageCard.jsx
+++
@@ -1,8 +0,0 @@
-import './ImageCard.css'
-export default function ImageCard({src, alt}) {
-    return (
-        <div className="image-card-wrapper">
-            <img src={src} alt={alt} className="image-card-img"/>
-        </div>
-    )
-}
--- WebstormProjects/happybd/src/components/UI/MyButton/MyButton.jsx
+++
@@ -1,5 +0,0 @@
-export default function MyButton({id, className, type, children, onClick}) {
-    return (
-        <button id={id} className={`button ${className}`} type={type} onClick={onClick}>{children}</button>
-    )
-}
